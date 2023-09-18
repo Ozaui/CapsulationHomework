e80619d437f2b257b55eafe7c7757c2b1ffe86ab
Özay Melih Yıldız <ozay.m.yildiz@gmail.com>
2023-09-18 20:59:42 +0300

    incomplete

---
+++ CapsulationHomework/CapsulationHomework/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapsulationHomework
+{
+    internal class Employee
+    {
+        private string employeeTCNo;
+        private bool control = false;
+
+        public string EMPLOYEETCNO
+        {
+            get 
+            {
+                return employeeTCNo;
+            }
+            set
+            {
+                if (value.Length == 11)
+                {   
+                    for(int i = 0; i < value.Length; i++)
+                    {
+                        bool isNumber = char.IsNumber(value[i]);
+                        if (isNumber)
+                        {
+                            employeeTCNo = value;
+                        }
+                        else
+                        {
+                            control = false;
+                            break;
+                        }
+                    }
+                    if(control)
+                    {
+                        Console.WriteLine("The character you entered is incorrect");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("the value you entered is incorrect");
+                }
+            }
+        }
+    }
+}
